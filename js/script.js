1310e5c89bbdd640e2d1f6d760931162a4133767
luckyfullstack2023 <luckyfullstack2023@gmail.com>
2023-06-18 12:43:17 +0530

    L-49 from batch-2 assignment done

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+// I can write script tag here
+
+console.log("Hello from external file")
+
+var aa = 1;
+
+var bb = 10;
---
+++ js/text.js
@@ -0,0 +1,31 @@
+// 1. Variable declaration
+var x;  // statement 1
+
+
+// 2. variable Initialization
+var y;  //statement-2
+y =20;     //statement-3
+
+
+// 3.variable delcaration and Initialization 
+var z = 50;          //statement-4
+console.log(y + z);  //statement-5
+
+
+
+var c = 5;
+
+var d = c++;
+
+console.log('c =', c);
+console.log("d =", d);
+
+                // Assignment operator
+var e = 1;
+e += 1; //right to left
+console.log('e =',e);
+
+var f =5;
+// subtraction assignment operator
+f -= 1;
+console.log('f =',f);
